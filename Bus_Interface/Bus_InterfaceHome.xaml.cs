9e56c882175bb9ea80f454deb9b39611764b0d74
jons63 <jonasgarsten@gmail.com>
2020-05-22 21:24:11 +0200

    Template application code

---
+++ Bus_Interface/Bus_InterfaceHome.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bus_Interface
+{
+    /// <summary>
+    /// Interaction logic for Bus_InterfaceHome.xaml
+    /// </summary>
+    public partial class Bus_InterfaceHome : Page
+    {
+        public Bus_InterfaceHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            Serial expenseReportPage = new Serial(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(expenseReportPage);
+        }
+    }
+}
---
+++ Bus_Interface/Serial.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bus_Interface
+{
+    /// <summary>
+    /// Interaction logic for Serial.xaml
+    /// </summary>
+    public partial class Serial : Page
+    {
+        public Serial()
+        {
+            InitializeComponent();
+        }
+        // Custom constructor to pass expense report data
+        public Serial(object data) : this()
+        {
+            // Bind to expense report data.
+            this.DataContext = data;
+        }
+    }
+}
